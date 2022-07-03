09ee86e756b2b4fa706b67b7bc9ea161b5e6b0ce
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-07-03 20:40:54 +0300

    Initial commit

---
+++ taskmanager/main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+#class Task(models.Model):
+#    title = models.CharField('Name', max_length=50)
+#    task = models.TextField('Description')
+#
+#    def __sfr__(self):
+#        return self.title
