d7a5a928c78bfce3104f93f02b4181a00792e90a
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-07-04 23:45:56 +0300

    added tasks

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 
-#class Task(models.Model):
-#    title = models.CharField('Name', max_length=50)
-#    task = models.TextField('Description')
-#
-#    def __sfr__(self):
-#        return self.title
+class Task(models.Model):
+    title = models.CharField('Name', max_length=50)
+    task = models.TextField('Description')
+
+
+    def __str__(self):
+        return self.title
